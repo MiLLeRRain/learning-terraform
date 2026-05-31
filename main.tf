4522a57cfc538ef812329c54678782074f53ef94
MiLLeRRain <xxdezz@gmail.com>
2026-06-01 00:17:11 +1200

    Refactor security group configuration for blog instance to use module

--- main.tf
+++ main.tf
@@ -22,51 +22,23 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.module_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow HTTP, HTTPS and SSH traffic in, and all outbound traffic out"
-  vpc_id      = data.aws_vpc.default.id
-}
+module "module_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+  name    = "blog_new_with_module"
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.blog.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+  vpc_id  = data.aws_vpc.default.id
 
-resource "aws_security_group_rule" "blog_https_in" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.blog.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+  ingress_rules = [http-80-tcp, https-443-tcp, ssh-22-tcp]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-resource "aws_security_group_rule" "blog_ssh_in" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = aws_security_group.blog.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.blog.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  egress_rules  = [all-all]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
