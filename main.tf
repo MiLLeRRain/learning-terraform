b2dab5578e3f56b2cd8ba2d372c5ee149c0d441d
MiLLeRRain <xxdezz@gmail.com>
2026-06-01 22:10:05 +1200

    Refactor AMI retrieval and environment variables for improved configurability

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,25 +11,39 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owners]
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
+}
+
+module "module_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+  name    = "blog_new_with_module"
+
+  vpc_id  = module.blog_vpc.vpc_id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules  = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 module "alb" {
@@ -52,7 +66,7 @@ module "alb" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -68,8 +82,8 @@ module "blog_autoscaling" {
   version = "9.2.1"
   name    = "blog"
 
-  min_size = 1
-  max_size = 2
+  min_size = var.min_size
+  max_size = var.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
@@ -97,18 +111,4 @@ module "blog_autoscaling" {
       traffic_source_identifier = aws_lb_target_group.blog.arn
     }
   }
-}
-
-module "module_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.1"
-  name    = "blog_new_with_module"
-
-  vpc_id  = module.blog_vpc.vpc_id
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules  = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
 }
